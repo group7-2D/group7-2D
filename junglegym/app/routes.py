cef8d6c9f8e7f3a108dd812c3d1c183ea02bbae8
shrons29 <118651552+shrons29@users.noreply.github.com>
2022-11-25 20:37:56 +0800

    Delete junglegym directory

--- junglegym/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app import application
-
-@application.route('/')
-@application.route('/demo')
-def demo():
-    return render_template('demo.html', title='Home')
