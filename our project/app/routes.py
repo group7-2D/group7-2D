1c7ba8674b75661e175f91b7448a5006a4624224
shrons29 <118651552+shrons29@users.noreply.github.com>
2022-11-25 20:01:38 +0800

    Delete our project directory

--- our project/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app import application
-
-@application.route('/')
-@application.route('/index')
-def index():
-    return render_template('index2.html', title='2D Project Group 7')
